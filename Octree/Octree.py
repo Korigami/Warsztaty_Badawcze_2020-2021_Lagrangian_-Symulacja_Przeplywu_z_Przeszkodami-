560b560a5fdb7e02d9ce263066f8cd46b382b43b
Michał Dybowski <44713520+mdybowski@users.noreply.github.com>
2021-04-18 21:54:23 +0200

    Poprawa budowy drzewa
    Dodanie atrybutów aktualnej wysokości drzewa i maksymalnej wysokości, brak zbędnych wierzchołków

--- Octree/Octree.py
+++ Octree/Octree.py
@@ -221,7 +221,6 @@ def Akeine_Moller_triangle_cube_intersection_test(cube_origin, cube_r, triangle_
 
     return True
 
-
 class Cube:
             
     def __init__(self, origin, r):
@@ -235,42 +234,57 @@ class Cube:
         triangle_normal = triangles_mesh.normals[triangle_idx]
         return Akeine_Moller_triangle_cube_intersection_test(self.origin, self.r,
                                                              triangle_p1, triangle_p2, triangle_p3, triangle_normal)
+    
+    def build_subcube(self, i, j, k):
+        new_cube_r = self.r/2
+        new_cube_origin = self.origin + (i*new_cube_r, j*new_cube_r, k*new_cube_r)
+        return Cube(new_cube_origin, new_cube_r)
         
     def __repr__(self):
         return 'Origin: {}, r = {}'.format(self.origin, self.r)
     
 class Node:
         
-    def __init__(self, cube):   
+    def __init__(self, cube, level):   
         self.triangles = []
         self.children = []
+        self.level = level
         self.cube = cube
+        self.subcubes = []
+        self.subcubes_node_exists = []
         
     def is_leaf(self):
          return not self.children
     
     def is_full(self, max_leaf_size):
           return len(self.triangles) == max_leaf_size
+    
+    def add_child(self, cube):
+        child = Node(cube, self.level+1)
+        self.children.append(child)
+        return child
         
     def split(self, triangles_mesh):
         for i in [-1, 1]:
             for j in [-1, 1]:
-                  for k in [-1, 1]:
-                    new_cube_r = self.cube.r/2
-                    new_cube_origin = self.cube.origin + (i*new_cube_r, j*new_cube_r, k*new_cube_r)
-                    curr_cube = Cube(new_cube_origin, new_cube_r)
-                    new_node = Node(curr_cube)
+                for k in [-1, 1]:
+                    curr_subcube = self.cube.build_subcube(i, j, k)
+                    self.subcubes.append(curr_subcube)              
+                    new_node_exists = False
                     for triangle_idx in self.triangles:  
-                        if curr_cube.intersects_triangle(triangle_idx, triangles_mesh):
+                        if curr_subcube.intersects_triangle(triangle_idx, triangles_mesh):
+                            if not new_node_exists:
+                                new_node = self.add_child(curr_subcube)
+                                new_node_exists = True
                             new_node.triangles.append(triangle_idx)
-                    self.children.append(new_node)
-        self.triangles.clear()
+                    self.subcubes_node_exists.append(new_node_exists*len(self.children) - 1)
+        self.triangles.clear() 
     
     def for_print(self, node, space=1):
         children_str = ''
         for child in node.children:
             children_str += (chr(10) + space*chr(9) + self.for_print(child, space+1))
-        return repr(node.cube) + ', triangles = ' + repr(node.triangles) + children_str
+        return repr(node.cube) + ', level = {}'.format(node.level) + ', triangles = ' + repr(node.triangles) + children_str
             
     def __repr__(self):
         return self.for_print(self)
@@ -279,10 +293,22 @@ class Node:
 class Octree:  
             
     
-    def __init__(self, init_cube, max_leaf_size, triangles_mesh):
-        self.root = Node(init_cube)
+    def __init__(self, init_cube, max_leaf_size, max_height, triangles_mesh):
+        self.root = Node(init_cube, 1)
+        self.height = 1
+        self.max_height = max_height
         self.max_leaf_size = max_leaf_size
         self.triangles_mesh = triangles_mesh
+        
+    def add_triangle_to_not_leaf(self, node, triangle_idx):
+        for subcube_idx in range(len(node.subcubes)):
+            if node.subcubes[subcube_idx].intersects_triangle(triangle_idx, self.triangles_mesh):
+                if node.subcubes_node_exists[subcube_idx] > -1:
+                    self.insert(triangle_idx, node.children[node.subcubes_node_exists[subcube_idx]])
+                else:
+                    new_node = node.add_child(node.subcubes[subcube_idx])
+                    new_node.triangles.append(triangle_idx)   
+                    node.subcubes_node_exists[subcube_idx] = len(node.children) -1
     
     def insert(self, triangle_idx, node=None):
         
@@ -293,12 +319,12 @@ class Octree:
                 self.insert(triangle_idx, self.root)
             
         elif not node.is_leaf():
-            for child in node.children:
-                if child.cube.intersects_triangle(triangle_idx, self.triangles_mesh):
-                    self.insert(triangle_idx, child)
+            self.add_triangle_to_not_leaf(node, triangle_idx)
                     
-        elif node.is_full(self.max_leaf_size):
+        elif node.is_full(self.max_leaf_size) and not node.level == self.max_height:
             node.split(self.triangles_mesh)
+            if node.level == self.height:
+                self.height += 1
             self.insert(triangle_idx, node)
             
         else:
@@ -309,24 +335,19 @@ class Octree:
             self.insert(triangle_idx)
             
     def __repr__(self):
-        return repr(self.root)
+        return 'Height = {}'.format(self.height) + chr(10) + repr(self.root)  
 
 
 # Bunny settings
 bunny_origin = np.array([30, 30, 30])
 bunny_r = 90
-maximal_leaf_size = 6
+maximal_leaf_size = 10
+maximal_tree_height = 10
 bunny_mesh = mesh.Mesh.from_file('data\Stanford_Bunny_sample.stl')
 
-
 bunny_cube = Cube(bunny_origin, bunny_r)
-octree = Octree(bunny_cube, maximal_leaf_size, bunny_mesh)
+octree = Octree(bunny_cube, maximal_leaf_size, maximal_tree_height, bunny_mesh)
 
-for i in range(30):
-    octree.insert(i)
+octree.build() # Przy tych danych budowa trwa okolo 4 min
 
 print(octree)
-
-
-# octree.build() do poprawy - czasami infinite loop
-## octree.build() 
