3c18e49d73aa2efc64e2cd05c50f05fa35144669
Michał Dybowski <44713520+mdybowski@users.noreply.github.com>
2021-04-23 00:39:53 +0200

    Dostosowanie pliku do testów zgodnie z nowym pakietem

--- Octree/Octree_test.py
+++ Octree/Octree_test.py
@@ -1,3 +1,9 @@
+import octree as oc
+import numpy as np
+from stl import mesh
+#import sys
+#np.set_printoptions(threshold=sys.maxsize)
+
 # Ustawienia do siatki krolika
 bunny_origin = np.array([30, 30, 30], dtype=np.double)
 bunny_r = 90
@@ -6,25 +12,33 @@ bunny_r = 90
 maximal_leaf_size = 10
 maximal_tree_height = 10
 
-path_to_file = 'Stanford_Bunny_sample.stl'
+path_to_file = 'data/Stanford_Bunny_sample.stl'
 bunny_mesh = mesh.Mesh.from_file(path_to_file)
 
-bunny_cube = Cube(bunny_origin, bunny_r)
-octree = Octree(bunny_cube, maximal_leaf_size, maximal_tree_height, bunny_mesh)
+bunny_cube = oc.Cube(bunny_origin, bunny_r)
+octree = oc.Octree(bunny_cube, maximal_leaf_size, maximal_tree_height, bunny_mesh)
 
-# Budowa drzewa
+# Budowa drzewa (mozna podac jako argument liczbe trojkatow, z ktorych chcemy zbudowac drzewo
+# np. octree.build(1000) zbuduje drzewo tylko z pierwszych 100 trojkatow) - przydatne do testow
+# Brak argumentu oznacza budowe drzewa z wszystkich dostepnych trojkatow
 octree.build()
 
-print(octree)
+#with open('bunny_octree.txt', 'a') as f:
+#    f.write(repr(octree))
 
 # Fejkowe czasteczki
 num_of_particles = 5000000
-particles = np.array(180*np.random.random_sample((num_of_particles,3)) - 60, dtype=np.double)
+particles = np.array(180*np.random.random_sample((num_of_particles, 3)) - 60, dtype=np.double)
 
 # Macierz trojkatow
 found_triangles = octree.search(particles)
 
-# Przekonwertowana macierz trojaktow (byc moze latwiejsza do wykorzystania niz powyzsza macierz, a liczy sie szybko - DO USTALENIA CZY KORZYSTAMY)
-converted_found_triangles = convert_found_triangles(searched_particles)
+#with open('bunny_triangles_matrix.txt', 'a') as f:
+#    f.write(repr(found_triangles))
 
-print(converted_found_triangles)
+# Przekonwertowana macierz trojaktow (byc moze latwiejsza do wykorzystania niz powyzsza macierz,
+# a liczy sie szybko - DO USTALENIA CZY KORZYSTAMY)
+converted_found_triangles = oc.convert_found_triangles(found_triangles)
+
+#with open('bunny_triangles_matrix_converted.txt', 'a') as f:
+#    f.write(repr(converted_found_triangles))
