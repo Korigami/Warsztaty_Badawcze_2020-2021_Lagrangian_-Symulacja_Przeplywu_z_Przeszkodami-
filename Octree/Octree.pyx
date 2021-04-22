3126696bdb337222539ddc0d7a0b1322f55ae918
Michał Dybowski <44713520+mdybowski@users.noreply.github.com>
2021-04-23 00:43:49 +0200

    Dostosowanie klasy octree do instalacji

--- Octree/Octree.pyx
+++ Octree/Octree.pyx
@@ -1,17 +1,12 @@
-import cython
-%load_ext cython
-
-%%cython
 #cython: boundscheck=False, wraparound=False, initializedcheck=False, cdivision=True
-cimport cython 
+cimport cython
 import numpy as np
 cimport numpy as np
-from stl import mesh
 
 cdef np.ndarray[np.double_t,ndim=1] find_min_max(double x0, double x1, double x2):
     cdef double p_min = x0
     cdef double p_max = x0
-        
+
     if x1 < p_min:
         p_min = x1
     if x1 > p_max:
@@ -20,7 +15,7 @@ cdef np.ndarray[np.double_t,ndim=1] find_min_max(double x0, double x1, double x2
         p_min = x2
     if x2 > p_max:
         p_max = x2
-            
+
     cdef np.ndarray[np.double_t,ndim=1] p = np.array([p_min, p_max], dtype=np.double)
     return p
 
@@ -40,25 +35,28 @@ cpdef int plane_cube_intersection(np.ndarray[np.double_t,ndim=1] vert, double cu
         return 0
     if np.dot(normal, vmax) >= 0:
         return 1
-        
+
     return 0
 
-cpdef int Akeine_Moller_triangle_cube_intersection_test(np.ndarray[np.double_t,ndim=1] cube_origin, double cube_r, np.ndarray[np.double_t,ndim=1] triangle_p1,
-                                                         np.ndarray[np.double_t,ndim=1] triangle_p2, np.ndarray[np.double_t,ndim=1] triangle_p3, np.ndarray[np.double_t,ndim=1] triangle_normal):
+cpdef int Akeine_Moller_triangle_cube_intersection_test(np.ndarray[np.double_t,ndim=1] cube_origin,
+                                                        double cube_r, np.ndarray[np.double_t,ndim=1] triangle_p1,
+                                                        np.ndarray[np.double_t,ndim=1] triangle_p2,
+                                                        np.ndarray[np.double_t,ndim=1] triangle_p3,
+                                                        np.ndarray[np.double_t,ndim=1] triangle_normal):
     ### Test Tomasa Akeine-Mollera na przeciecie trojkata z kostka        ###
     ### https://fileadmin.cs.lth.se/cs/Personal/Tomas_Akenine-Moller/code ###
     ### Przepisane z C do Python                                          ###
-        
+
     cdef np.ndarray[np.double_t,ndim=1] v0 = triangle_p1 - cube_origin
     cdef np.ndarray[np.double_t,ndim=1] v1 = triangle_p2 - cube_origin
     cdef np.ndarray[np.double_t,ndim=1] v2 = triangle_p3 - cube_origin
-        
+
     cdef np.ndarray[np.double_t,ndim=1] e0 = v1 - v0
     cdef np.ndarray[np.double_t,ndim=1] e1 = v2 - v1
     cdef np.ndarray[np.double_t,ndim=1] e2 = v0 - v2
-    
+
     ###### Bullet 3 #####
-    
+
     cdef np.ndarray[np.double_t,ndim=1] fe0 = np.abs(e0)
     cdef np.ndarray[np.double_t,ndim=1] fe1 = np.abs(e1)
     cdef np.ndarray[np.double_t,ndim=1] fe2 = np.abs(e2)
@@ -73,12 +71,12 @@ cpdef int Akeine_Moller_triangle_cube_intersection_test(np.ndarray[np.double_t,n
     cdef np.ndarray[np.double_t,ndim=1] p
 
     cdef double rad
-        
+
     #### Test 1 ####
-        
+
     p0 = e0[2]*v0[1] - e0[1]*v0[2]
     p2 = e0[2]*v2[1] - e0[1]*v2[2]
-        
+
     if p0 < p2:
         p_min = p0
         p_max = p2
@@ -88,12 +86,12 @@ cpdef int Akeine_Moller_triangle_cube_intersection_test(np.ndarray[np.double_t,n
     rad = (fe0[2] + fe0[1])*cube_r
     if p_min > rad or p_max < -rad:
         return 0
-  
+
     #### Test 2 ####
-        
+
     p0 = -e0[2]*v0[0] + e0[0]*v0[2]
     p2 = -e0[2]*v2[0] + e0[0]*v2[2]
-        
+
     if p0 < p2:
         p_min = p0
         p_max = p2
@@ -105,10 +103,10 @@ cpdef int Akeine_Moller_triangle_cube_intersection_test(np.ndarray[np.double_t,n
         return 0
 
     #### Test 3 ####
-        
+
     p1 = e0[1]*v1[0] - e0[0]*v1[1]
     p2 = e0[1]*v2[0] - e0[0]*v2[1]
-        
+
     if p2 < p1:
         p_min = p2
         p_max = p1
@@ -120,10 +118,10 @@ cpdef int Akeine_Moller_triangle_cube_intersection_test(np.ndarray[np.double_t,n
         return 0
 
     #### Test 4 ####
-        
+
     p0 = e1[2]*v0[1] - e1[1]*v0[2]
     p2 = e1[2]*v2[1] - e1[1]*v2[2]
-        
+
     if p0 < p2:
         p_min = p0
         p_max = p2
@@ -135,10 +133,10 @@ cpdef int Akeine_Moller_triangle_cube_intersection_test(np.ndarray[np.double_t,n
         return 0
 
     #### Test 5 ####
-        
+
     p0 = -e1[2]*v0[0] + e1[0]*v0[2]
     p2 = -e1[2]*v2[0] + e1[0]*v2[2]
-        
+
     if p0 < p2:
         p_min = p0
         p_max = p2
@@ -150,10 +148,10 @@ cpdef int Akeine_Moller_triangle_cube_intersection_test(np.ndarray[np.double_t,n
         return 0
 
     #### Test 6 ####
-        
+
     p0 = e1[1]*v0[0] - e1[0]*v0[1]
     p1 = e1[1]*v1[0] - e1[0]*v1[1]
-        
+
     if p0 < p1:
         p_min = p0
         p_max = p2
@@ -165,10 +163,10 @@ cpdef int Akeine_Moller_triangle_cube_intersection_test(np.ndarray[np.double_t,n
         return 0
 
     #### Test 7 ####
-        
+
     p0 = e2[2]*v0[1] - e2[1]*v0[2]
     p1 = e2[2]*v1[1] - e2[1]*v1[2]
-        
+
     if p0 < p1:
         p_min = p0
         p_max = p1
@@ -180,10 +178,10 @@ cpdef int Akeine_Moller_triangle_cube_intersection_test(np.ndarray[np.double_t,n
         return 0
 
     #### Test 8 ####
-        
+
     p0 = -e2[2]*v0[0] + e2[0]*v0[2]
     p1 = -e2[2]*v1[0] + e2[0]*v1[2]
-        
+
     if p0 < p1:
         p_min = p0
         p_max = p1
@@ -195,10 +193,10 @@ cpdef int Akeine_Moller_triangle_cube_intersection_test(np.ndarray[np.double_t,n
         return 0
 
     #### Test 9 ####
-        
+
     p1 = e2[1]*v1[0] - e2[0]*v1[1]
     p2 = e2[1]*v2[0] - e2[0]*v2[1]
-        
+
     if p2 < p1:
         p_min = p2
         p_max = p1
@@ -210,38 +208,38 @@ cpdef int Akeine_Moller_triangle_cube_intersection_test(np.ndarray[np.double_t,n
         return 0
 
     ##### Bullet 2 ######
-    
-    ### Test 10 ### 
+
+    ### Test 10 ###
 
     p = find_min_max(v0[0], v1[0], v2[0])
     p_min = p[0]
     p_max = p[1]
-    
+
     if p_min > cube_r or p_max < -cube_r:
         return 0
 
     ### Test 11 ###
-    
+
     p = find_min_max(v0[1], v1[1], v2[1])
     p_min = p[0]
     p_max = p[1]
-    
+
     if p_min > cube_r or p_max < -cube_r:
         return 0
 
     ### Test 12 ###
-    
+
     p = find_min_max(v0[2], v1[2], v2[2])
     p_min = p[0]
     p_max = p[1]
-    
+
     if p_min > cube_r or p_max < -cube_r:
         return 0
 
     ##### Bullet 1 ######
-    
+
     ### Test 13 ###
-    
+
     if not plane_cube_intersection(v0, cube_r, triangle_normal):
         return 0
 
@@ -252,10 +250,10 @@ cdef class Cube:
     cdef public:
         origin
         double r
-            
+
     def __cinit__(self, np.ndarray[np.double_t,ndim=1] origin, double r):
         self.origin = origin
-        self.r = r  
+        self.r = r
 
     cpdef int intersects_triangle(self, int triangle_idx, np.ndarray[np.double_t,ndim=2] triangles_mesh_v0,
                                   np.ndarray[np.double_t,ndim=2] triangles_mesh_v1,
@@ -266,7 +264,7 @@ cdef class Cube:
         cdef np.ndarray[np.double_t,ndim=1] triangle_p3 = triangles_mesh_v2[triangle_idx]
         cdef np.ndarray[np.double_t,ndim=1] triangle_normal = triangles_mesh_normals[triangle_idx]
         return Akeine_Moller_triangle_cube_intersection_test(self.origin, self.r,
-                                                             triangle_p1, triangle_p2, triangle_p3, triangle_normal) 
+                                                             triangle_p1, triangle_p2, triangle_p3, triangle_normal)
 
     cdef Cube build_subcube(self, int i, int j, int k):
         cdef double new_cube_r = self.r/2
@@ -289,7 +287,7 @@ cdef class Node:
         list subcubes
         list subcubes_node_exists
 
-    def __cinit__(self, Cube cube, int level):  
+    def __cinit__(self, Cube cube, int level):
         self.triangles = []
         self.children = []
         self.level = level
@@ -323,9 +321,9 @@ cdef class Node:
             for j in [-1, 1]:
                 for k in [-1, 1]:
                     curr_subcube = self.cube.build_subcube(i, j, k)
-                    self.subcubes.append(curr_subcube)              
+                    self.subcubes.append(curr_subcube)
                     new_node_exists = 0
-                    for triangle_idx in self.triangles:  
+                    for triangle_idx in self.triangles:
                         if curr_subcube.intersects_triangle(triangle_idx, triangles_mesh_v0,
                                                             triangles_mesh_v1, triangles_mesh_v2, triangles_mesh_normals):
                             if not new_node_exists:
@@ -333,7 +331,7 @@ cdef class Node:
                                 new_node_exists = 1
                             new_node.triangles.append(triangle_idx)
                     self.subcubes_node_exists.append(new_node_exists*len(self.children) - 1)
-        self.triangles.clear() 
+        self.triangles.clear()
 
     cdef str for_print(self, Node node, int space=1):
         cdef str children_str = ''
@@ -341,7 +339,7 @@ cdef class Node:
         for child in node.children:
             children_str += (chr(10) + space*chr(9) + self.for_print(child, space+1))
         return repr(node.cube) + ', level = {}'.format(node.level) + ', triangles = ' + repr(node.triangles) + children_str
-            
+
     def __repr__(self):
         return self.for_print(self)
 
@@ -378,7 +376,7 @@ cdef class Octree:
                     self.insert(triangle_idx, node.children[node.subcubes_node_exists[subcube_idx]])
                 else:
                     new_node = node.add_child(node.subcubes[subcube_idx])
-                    new_node.triangles.append(triangle_idx)   
+                    new_node.triangles.append(triangle_idx)
                     node.subcubes_node_exists[subcube_idx] = len(node.children) -1
 
     cdef void insert(self, int triangle_idx, Node node=None):
@@ -387,10 +385,10 @@ cdef class Octree:
                 self.root.triangles.append(triangle_idx)
             else:
                 self.insert(triangle_idx, self.root)
-            
+
         elif not node.is_leaf():
             self.add_triangle_to_not_leaf(node, triangle_idx)
-                    
+
         elif node.is_full(self.max_leaf_size):
             if not node.level == self.max_height:
                 node.split(self.triangles_mesh_v0, self.triangles_mesh_v1, self.triangles_mesh_v2, self.triangles_mesh_normals)
@@ -401,13 +399,17 @@ cdef class Octree:
                 node.triangles.append(triangle_idx)
                 if len(node.triangles) > self.real_max_leaf_size:
                     self.real_max_leaf_size += 1
-            
+
         else:
             node.triangles.append(triangle_idx)
-            
-    cpdef void build(self):
+
+    cpdef void build(self, int how_many_triangles=-1):
         cdef int triangle_idx
-        for triangle_idx in range(len(self.triangles_mesh_v0)):
+        if how_many_triangles == -1:
+            how_many_triangles = len(self.triangles_mesh_v0)
+        else:
+            how_many_triangles = min(len(self.triangles_mesh_v0), how_many_triangles)
+        for triangle_idx in range(how_many_triangles):
             self.insert(triangle_idx)
 
     cdef np.ndarray[np.int32_t,ndim=2] for_search(self, np.ndarray[np.double_t,ndim=2] particles,
