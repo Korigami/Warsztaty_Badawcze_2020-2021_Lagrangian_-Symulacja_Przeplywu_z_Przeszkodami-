6bde5e3f2a8a76023ecb372be95af8155a14af98
Michał Dybowski <44713520+mdybowski@users.noreply.github.com>
2021-05-28 03:00:25 +0200

    Update time_test.py

--- Octree/time_test.py
+++ Octree/time_test.py
@@ -10,14 +10,14 @@ path_to_file = 'data/Stanford_Bunny_sample.stl'
 bunny_mesh = mesh.Mesh.from_file(path_to_file)
 bunny_cube = ot.Cube(bunny_origin, bunny_r)
 
-build_time = [[], [], []]
-search_time = [[], [], []]
+build_time = [[], [], [], []]
+search_time = [[], [], [], []]
 height_list = [5, 7, 9, 10]
-maximal_leaf_size_list = [10, 20, 50]
+maximal_leaf_size_list = [10, 20, 50, 200]
 num_of_particles = 500000
 particles = np.array(180*np.random.random_sample((num_of_particles, 3)) - 60, dtype=np.double)
 
-for idx in range(3):
+for idx in range(4):
     for height in height_list:
         maximal_tree_height = height
         bunny_octree = ot.Octree(bunny_cube, maximal_leaf_size_list[idx], maximal_tree_height, bunny_mesh)
@@ -38,6 +38,7 @@ figure, axis = plt.subplots(1, 2)
 axis[0].plot(height_list, build_time[0], label='leaf_size=10')
 axis[0].plot(height_list, build_time[1], label='leaf_size=20')
 axis[0].plot(height_list, build_time[2], label='leaf_size=50')
+axis[0].plot(height_list, build_time[3], label='leaf_size=200')
 
 axis[0].set_title('Czas budowy drzewa')
 axis[0].set_xlabel('Wysokosc drzewa')
@@ -46,6 +47,7 @@ axis[0].set_ylabel('Czas')
 axis[1].plot(height_list, search_time[0], label='leaf_size=10')
 axis[1].plot(height_list, search_time[1], label='leaf_size=20')
 axis[1].plot(height_list, search_time[2], label='leaf_size=50')
+axis[1].plot(height_list, search_time[3], label='leaf_size=200')
 
 axis[1].set_title('Czas przeszukiwania drzewa')
 axis[1].set_xlabel('Wysokosc drzewa')
