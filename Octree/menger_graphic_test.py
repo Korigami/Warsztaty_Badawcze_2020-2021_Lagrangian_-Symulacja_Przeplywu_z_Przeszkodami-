d5a41f7783585314f02cbdc4111b5236dceb1ba7
Michał Dybowski <44713520+mdybowski@users.noreply.github.com>
2021-06-01 18:39:18 +0200

    Create menger_graphic_test.py

---
+++ Octree/menger_graphic_test.py
@@ -0,0 +1,26 @@
+import octree as ot
+import numpy as np
+from stl import mesh
+
+menger_origin = np.array([0, 0, 0], dtype=np.double)
+menger_r = 2
+
+maximal_leaf_size = 20
+maximal_tree_height = 7
+
+path_to_file = 'data/Menger_sponge_sample.stl'
+menger_mesh = mesh.Mesh.from_file(path_to_file)
+
+menger_cube = ot.Cube(menger_origin, menger_r)
+menger_octree = ot.Octree(menger_cube, maximal_leaf_size, maximal_tree_height, menger_mesh)
+
+menger_octree.build()
+
+plotter = menger_octree.plotter_cubes_mesh()
+
+plotter.show()
+
+
+
+
+
