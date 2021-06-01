a95e78a5ba2cb7021496b38b41a89785105254ae
Michał Dybowski <44713520+mdybowski@users.noreply.github.com>
2021-06-01 20:06:14 +0200

    Update menger_time_test.py

--- Octree/menger_time_test.py
+++ Octree/menger_time_test.py
@@ -12,12 +12,12 @@ menger_cube = ot.Cube(menger_origin, menger_r)
 
 build_time = [[], [], [], []]
 search_time = [[], [], [], []]
-height_list = [5, 7, 9, 10]
-maximal_leaf_size_list = [5, 10, 15]
+height_list = [7, 9, 10, 12]
+maximal_leaf_size_list = [10, 20, 30, 50]
 num_of_particles = 500000
-particles = np.array(180*np.random.random_sample((num_of_particles, 3)) - 60, dtype=np.double)
+particles = np.array(np.random.random_sample((num_of_particles, 3)), dtype=np.double)
 
-for idx in range(3):
+for idx in range(4):
     for height in height_list:
         maximal_tree_height = height
         menger_octree = ot.Octree(menger_cube, maximal_leaf_size_list[idx], maximal_tree_height, menger_mesh)
@@ -36,17 +36,19 @@ for idx in range(3):
 figure, axis = plt.subplots(1, 2)
 figure.set_size_inches(18.5, 10.5)
 
-axis[0].plot(height_list, build_time[0], label='maximal_leaf_size=5')
-axis[0].plot(height_list, build_time[1], label='maximal_leaf_size=10')
-axis[0].plot(height_list, build_time[2], label='maximal_leaf_size=15')
+axis[0].plot(height_list, build_time[0], label='maximal_leaf_size=10')
+axis[0].plot(height_list, build_time[1], label='maximal_leaf_size=20')
+axis[0].plot(height_list, build_time[2], label='maximal_leaf_size=30')
+axis[0].plot(height_list, build_time[3], label='maximal_leaf_size=50')
 
 axis[0].set_title('Czas budowy drzewa')
 axis[0].set_xlabel('Wysokosc drzewa')
 axis[0].set_ylabel('Czas')
 
-axis[1].plot(height_list, search_time[0], label='maximal_leaf_size=5')
-axis[1].plot(height_list, search_time[1], label='maximal_leaf_size=10')
-axis[1].plot(height_list, search_time[2], label='maximal_leaf_size=15')
+axis[1].plot(height_list, search_time[0], label='maximal_leaf_size=10')
+axis[1].plot(height_list, search_time[1], label='maximal_leaf_size=20')
+axis[1].plot(height_list, search_time[2], label='maximal_leaf_size=30')
+axis[1].plot(height_list, search_time[3], label='maximal_leaf_size=50')
 
 axis[1].set_title('Czas przeszukiwania drzewa (500 tys. czastek)')
 axis[1].set_xlabel('Wysokosc drzewa')
